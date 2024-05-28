e30ea757f639afc552e76e4bf7bcbe5381428856
Lucas Rigon <lucas@bussolasocial.com.br>
2024-05-27 22:02:25 -0300

    First commit

---
+++ src/main/java/com/fiap/msclienteapi/MsClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.msclienteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/msclienteapi/MsClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.msclienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
